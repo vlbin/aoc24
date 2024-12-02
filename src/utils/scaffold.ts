d99107b201afa2a67c5440eeb60d479df8862eae
albin <albinlandgreen@gmail.com>
2024-12-02 19:19:40 +0100

    change file endings

--- src/utils/scaffold.ts
+++ src/utils/scaffold.ts
@@ -3,19 +3,12 @@ import template from './template';
 
 const basePath = 'src/days';
 
-const url = 'https://adventofcode.com/2024/day';
 const day = Bun.argv[2] ? Number(Bun.argv[2]) : new Date().getDate();
 const padded = day < 10 ? '0' + day : day;
 
-const getInputData = (day: number) =>
-  fetch(`${url}/${day}/input`).then((res) => res.text());
-
 mkdirSync(`${basePath}/${padded}`);
 
-Bun.write(`${basePath}/${padded}/test.txt`, '');
-
-getInputData(day).then((data) => {
-  Bun.write(`${basePath}/${padded}/input.txt`, data);
-});
+Bun.write(`${basePath}/${padded}/test`, '');
+Bun.write(`${basePath}/${padded}/input`, '');
 
 Bun.write(`${basePath}/${padded}/solution.ts`, template);
