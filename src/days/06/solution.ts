edbc85b5d7fe76635e66824dfa4476455fed9102
vlbin <albinlandgreen@gmail.com>
2024-12-06 13:19:33 +0100

    day 6 part 2

--- src/days/06/solution.ts
+++ src/days/06/solution.ts
@@ -10,22 +10,48 @@ const directions = {
 type Direction = typeof directions;
 const nextDir: (keyof Direction)[] = ['up', 'right', 'down', 'left'];
 
+const createRecord = (currentPos: [number, number]) => currentPos.join('-');
+
+const createRecordWithDir = (currentPos: [number, number], direction: keyof Direction) =>
+  [...currentPos, direction].join('-');
+
+const isLooping = (pos: [number, number], direction: keyof Direction, visited: Set<string>) =>
+  visited.has(createRecordWithDir(pos, direction));
+
 const visit = (
   currentPos: [number, number],
   direction: keyof Direction,
   matrix: string[][],
   visited: Set<string>,
 ): Set<string> => {
-  console.log(currentPos, direction);
-
   const [nextRow, nextCol] = [currentPos[0] + directions[direction][0], currentPos[1] + directions[direction][1]];
   const block = matrix[nextRow]?.[nextCol];
 
   return block
     ? block === '#'
       ? visit(currentPos, nextDir[(nextDir.indexOf(direction) + 1) % 4], matrix, visited)
-      : visit([nextRow, nextCol], direction, matrix, visited.add(currentPos.join('-')))
-    : visited.add(currentPos.join('-'));
+      : visit([nextRow, nextCol], direction, matrix, visited.add(createRecord(currentPos)))
+    : visited.add(createRecord(currentPos));
+};
+
+const matrixToString = (matrix: string[][]) => matrix.map((row) => row.join('')).join('\n');
+
+const visitLoop = (
+  currentPos: [number, number],
+  direction: keyof Direction,
+  matrix: string[][],
+  visited: Set<string>,
+): number => {
+  const [nextRow, nextCol] = [currentPos[0] + directions[direction][0], currentPos[1] + directions[direction][1]];
+  const block = matrix[nextRow]?.[nextCol];
+
+  return isLooping(currentPos, direction, visited)
+    ? 1
+    : block
+      ? block === '#' || block === '0'
+        ? visitLoop(currentPos, nextDir[(nextDir.indexOf(direction) + 1) % 4], matrix, visited)
+        : visitLoop([nextRow, nextCol], direction, matrix, visited.add(createRecordWithDir(currentPos, direction)))
+      : 0;
 };
 
 export const part1 = (data: string) => {
@@ -36,4 +62,20 @@ export const part1 = (data: string) => {
   return visit([row, col], 'up', matrix, new Set()).size;
 };
 
-export const part2 = (data: string) => {};
+export const part2 = (data: string) => {
+  const matrix = grid(data);
+  const startRow = matrix.findIndex((row) => row.some((char) => char === '^'));
+  const startCol = matrix[startRow].findIndex((char) => char === '^');
+
+  const visited = visit([startRow, startCol], 'up', matrix, new Set());
+  const visitedCoords = [...visited]
+    .map((str) => str.split('-').slice(0, 2).map(Number))
+    .filter(([row, col]) => !(row === startRow && col === startCol));
+
+  return visitedCoords.reduce((count, [ri, ci]) => {
+    matrix[ri][ci] = '0';
+    count += visitLoop([startRow, startCol], 'up', matrix, new Set());
+    matrix[ri][ci] = '.';
+    return count;
+  }, 0);
+};
