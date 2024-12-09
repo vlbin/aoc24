d93687eb5923767cfee76d6778861bcfdecd947f
vlbin <albinlandgreen@gmail.com>
2024-12-09 23:20:32 +0100

    day 9 part 1 + part 2 wip

--- src/days/09/solution.ts
+++ src/days/09/solution.ts
@@ -1,34 +1,75 @@
+import { range, updateAt } from '@lib/array';
+
+const replicate = (value: string, times: number) => range(0, times).map((_) => value);
+
 const blocks = (map: ReadonlyArray<string>) =>
   map.reduce(
     ({ id, map }, el, idx) =>
       idx % 2 === 0
-        ? { id: id + 1, map: map.concat(''.padStart(Number(el), id.toString())) }
-        : { id: id, map: map.concat(''.padStart(Number(el), '.')) },
-    { id: 0, map: '' },
+        ? {
+            id: id + 1,
+            map: map.concat(replicate(id.toString(), Number(el))),
+          }
+        : { id: id, map: map.concat(replicate('.', Number(el))) },
+    { id: 0, map: [] as string[] },
   );
 
-const isReordered = (blocks: string, targetLength: number) =>
-  blocks.slice(blocks.indexOf('.')).length === blocks.length - targetLength;
+const blocksGrouped = (map: ReadonlyArray<string>) =>
+  map.reduce(
+    ({ id, map }, el, idx) =>
+      idx % 2 === 0
+        ? {
+            id: id + 1,
+            map: map.concat([replicate(id.toString(), Number(el))]),
+          }
+        : { id: id, map: map.concat([replicate('.', Number(el))]) },
+    { id: 0, map: [] as string[][] },
+  );
 
-const compress = (blocks: string, reordered: string, idx: number, targetLength: number): string => {
-  if (isReordered(reordered, targetLength)) return reordered;
+const compress = (
+  blocks: ReadonlyArray<string>,
+  reordered: ReadonlyArray<string>,
+  idx: number,
+  targetLength: number,
+): ReadonlyArray<string> => {
+  if (reordered.length === targetLength) return reordered;
 
-  const lastBlock = blocks.slice(-1);
+  const lastBlock = blocks.at(-1)!;
+  const value = blocks[idx];
 
   if (lastBlock === '.') return compress(blocks.slice(0, -1), reordered, idx, targetLength);
+  if (value === '.') return compress(blocks.slice(0, -1), reordered.concat(lastBlock), idx + 1, targetLength);
 
-  if (blocks[idx] === '.') return compress(blocks.slice(0, -1), reordered.concat(lastBlock), idx + 1, targetLength);
-
-  return compress(blocks, reordered.concat(blocks[idx]), idx + 1, targetLength);
+  return compress(blocks, reordered.concat(value), idx + 1, targetLength);
 };
 
 export const part1 = (data: string) => {
   const fileblocks = blocks(data.split('')).map;
-  const targetLength = fileblocks.split('').filter((block) => block !== '.').length;
 
-  const compressed = compress(fileblocks, '', 0, targetLength);
+  const targetLength = fileblocks.filter((block) => block !== '.').length;
 
-  return compressed.split('').reduce((acc, el, idx) => acc + Number(el) * idx, 0);
+  const compressed = compress(fileblocks, [], 0, targetLength);
+
+  return compressed.reduce((acc, el, idx) => (el === '.' ? acc : acc + Number(el) * idx), 0);
 };
 
-export const part2 = (data: string) => {};
+export const part2 = (data: string) => {
+  const fileblocks = blocksGrouped(data.split('')).map.filter((group) => group.length);
+
+  const reversed = fileblocks.toReversed();
+  const reordered = reversed.reduce((reordered, group) => {
+    const firstFitting = fileblocks.findIndex((free) => free[0] === '.' && free.length >= group.length);
+
+    if (firstFitting === -1) {
+      return reordered;
+    }
+
+    return updateAt(firstFitting)(() => group.join('').padEnd(reordered[firstFitting].length, '.').split(''))(
+      reordered,
+    );
+  }, fileblocks as string[][]);
+
+  console.log(reordered);
+
+  return;
+};
